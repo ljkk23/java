7acce0a62652eeb7ee182b433447ac224551e271
lj <2156718743@qq.com>
2022-07-31 15:34:32 +0800

    同步

---
+++ fileSystem/src/test/java/GeometricObject/Triangle.java
@@ -0,0 +1,40 @@
+package GeometricObject;
+
+public class Triangle extends GeometricObject {
+	private double side1;
+	private double side2;
+	private double side3;
+	public Triangle(double side1, double side2, double side3) {
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
+	public double getSide1() {
+		return side1;
+	}
+	public void setSide1(double side1) {
+		this.side1 = side1;
+	}
+	public double getSide2() {
+		return side2;
+	}
+	public void setSide2(double side2) {
+		this.side2 = side2;
+	}
+	public double getSide3() {
+		return side3;
+	}
+	public void setSide3(double side3) {
+		this.side3 = side3;
+	}
+	public double getArea() {
+		double s = (side1 + side2 + side3) / 2;
+		return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+	}
+	public double getPerimeter() {
+		return side1 + side2 + side3;
+	}
+}
+————————————————
+版权声明：本文为CSDN博主「「已注销」」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/tfboys_3/article/details/105805844
---
+++ fileSystem/src/test/java/animal/Text.java
@@ -0,0 +1,10 @@
+package animal;
+
+public class Text {
+
+	public static void main(String[] args) {
+		Simulator simulator=new Simulator();
+		simulator.playSound(new Dog());
+		simulator.playSound(new Cat());
+	}
+}
