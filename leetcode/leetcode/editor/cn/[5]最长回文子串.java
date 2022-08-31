6453679b487fe6ba43638be48a6ed088e80b07bc
lj <2156718743@qq.com>
2022-08-31 20:12:40 +0800

    新增leetcode中第5题，用动态规划寻找最大回文字符串

--- leetcode/leetcode/editor/cn/[5]最长回文子串.java
+++ leetcode/leetcode/editor/cn/[5]最长回文子串.java
@@ -1,8 +1,36 @@
+//给你一个字符串 s，找到 s 中最长的回文子串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "cbbd"
+//输出："bb"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 仅由数字和英文字母组成 
+// 
+//
+// Related Topics 字符串 动态规划 👍 5642 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public static void main(String[] args) {
-        String cbbd = longestPalindrome("cbbd");
-        System.out.println(cbbd);
-    }
     public static String longestPalindrome(String s) {
         int len=s.length();
         Boolean[][] dp=new Boolean[len][len];
@@ -17,7 +45,7 @@ class Solution {
                 return s;
             }
         }
-        int maxLenth=0;
+        int maxLenth=1;
         int begin=0;
         //这里一定要注意，要先确定列的值，因为如果chars[i]==char[j],就会状态转移到char[i+1]和char[j-1],所以必须有char[i+1]和char[j-1]的判断，所以就应该有列的数据
         for (int j = 1; j < len; j++) {
@@ -26,11 +54,11 @@ class Solution {
                 if (chars[i]!=chars[j]){
                     dp[i][j]=false;
                 }else {
-                        if (j - i < 3) {
-                            dp[i][j]=true;
-                        }else {
-                            dp[i][j]=dp[i+1][j-1];
-                        }
+                    if (j - i < 3) {
+                        dp[i][j]=true;
+                    }else {
+                        dp[i][j]=dp[i+1][j-1];
+                    }
                 }
                 if (dp[i][j] && maxLenth < j + 1 - i) {
                     maxLenth = j + 1 - i;
@@ -41,3 +69,4 @@ class Solution {
         return s.substring(begin,begin+maxLenth);
     }
 }
+//leetcode submit region end(Prohibit modification and deletion)
